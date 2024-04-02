39f99b96b768cebab2ee0e732d279b71c2900694
BraumAce <1693717911@qq.com>
2024-04-02 22:08:08 +0800

    Initial commit

---
+++ src/main/java/com/tolearn/ToLearnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToLearnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToLearnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tolearn/ToLearnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToLearnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
