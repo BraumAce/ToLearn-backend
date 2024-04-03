280ec4453f42ca338f2c9b3b524576215e31ad76
BraumAce <1693717911@qq.com>
2024-04-03 20:44:37 +0800

    用户队伍关系

---
+++ src/main/java/com/tolearn/Model/Domain/UserTeam.java
@@ -0,0 +1,48 @@
+package com.tolearn.Model.Domain;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户队伍关系
+ * @author BraumAce
+ */
+@Data
+@TableName(value = "user_team")
+@ApiModel(value = "用户队伍关系")
+public class UserTeam implements Serializable {
+    //主键
+    @TableId
+    @ApiModelProperty(value = "id")
+    private Long id;
+
+    @ApiModelProperty(value = "用户id")
+    private Long userId;
+
+    @ApiModelProperty(value = "队伍id")
+    private Long teamId;
+
+    @ApiModelProperty(value = "加入时间")
+    private Date joinTime;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "逻辑删除")
+    private Integer isDelete;
+
+    @Serial
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
