b969a46982baaaf68960e32c5588714ae12de438
BraumAce <1693717911@qq.com>
2024-04-03 20:42:29 +0800

    博文点赞

---
+++ src/main/java/com/tolearn/Model/Domain/BlogLike.java
@@ -0,0 +1,46 @@
+package com.tolearn.Model.Domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 博文点赞
+ * @author BraumAce
+ */
+@Data
+@TableName(value = "blog_like")
+@ApiModel(value = "博文点赞")
+public class BlogLike implements Serializable {
+    // 主键
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty(value = "id")
+    private Long id;
+
+    @ApiModelProperty(value = "博文id")
+    private Long blogId;
+
+    @ApiModelProperty(value = "用户id")
+    private Long userId;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "逻辑删除")
+    private Integer isDelete;
+
+    @Serial
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
