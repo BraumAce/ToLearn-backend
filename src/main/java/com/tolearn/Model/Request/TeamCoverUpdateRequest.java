f2f0100408f1fd2f979ccfeba0a1d667012f273f
BraumAce <1693717911@qq.com>
2024-04-03 21:17:44 +0800

    队伍封面更新请求

---
+++ src/main/java/com/tolearn/Model/Request/TeamCoverUpdateRequest.java
@@ -0,0 +1,21 @@
+package com.tolearn.Model.Request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 团队封面更新请求
+ * @author BraumAce
+ */
+@Data
+@ApiModel(value = "队伍封面更新请求")
+public class TeamCoverUpdateRequest {
+
+    @ApiModelProperty(value = "队伍id")
+    private Long id;
+
+    @ApiModelProperty(value = "封面文件")
+    private MultipartFile file;
+}
