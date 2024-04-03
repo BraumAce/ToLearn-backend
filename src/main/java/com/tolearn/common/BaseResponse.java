d01a3bb2030fe43f5f2f3ca0028c959c8920766e
BraumAce <1693717911@qq.com>
2024-04-03 19:52:11 +0800

    返回类

---
+++ src/main/java/com/tolearn/common/BaseResponse.java
@@ -0,0 +1,53 @@
+package com.tolearn.common;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 通用返回类
+ * @param <T>
+ * @author BraumAce
+ */
+@Data
+public class BaseResponse<T> implements Serializable {
+
+    @ApiModelProperty(value = "响应码")
+    private int code;
+
+
+    @ApiModelProperty(value = "数据")
+    private T data;
+
+
+    @ApiModelProperty(value = "消息")
+    private String message;
+
+
+    @ApiModelProperty(value = "描述")
+    private String description;
+
+    public BaseResponse(int code, T data, String message, String description) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+        this.description = description;
+    }
+
+    public BaseResponse(int code, T data, String message) {
+        this(code, data, message, "");
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "", "");
+    }
+
+    /**
+     * 返回报错信息
+     * @param errorCode 错误代码
+     */
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription());
+    }
+}
