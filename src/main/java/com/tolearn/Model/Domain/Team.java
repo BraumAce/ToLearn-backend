32db5814fcf85ba1081c4377d74f6621fc17a5db
BraumAce <1693717911@qq.com>
2024-04-03 20:44:07 +0800

    队伍

---
+++ src/main/java/com/tolearn/Model/Domain/Team.java
@@ -0,0 +1,66 @@
+package com.tolearn.Model.Domain;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 队伍
+ * @author BraumAce
+ */
+@Data
+@TableName(value = "team")
+@ApiModel(value = "队伍")
+public class Team implements Serializable {
+    // 主键
+    @TableId
+    @ApiModelProperty(value = "id")
+    private Long id;
+
+    @ApiModelProperty(value = "队伍名称")
+    private String name;
+
+    @ApiModelProperty(value = "描述")
+    private String description;
+
+    @ApiModelProperty(value = "封面图片")
+    private String coverImage;
+
+    @ApiModelProperty(value = "最大人数")
+    private Integer maxNum;
+
+    @ApiModelProperty(value = "过期时间")
+    private Date expireTime;
+
+    @ApiModelProperty(value = "队长id")
+    private Long userId;
+
+    // 状态 0 - 公开，1 - 私有，2 - 加密
+    @ApiModelProperty(value = "状态 0 - 公开，1 - 私有，2 - 加密")
+    private Integer status;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @TableLogic
+    @ApiModelProperty(value = "逻辑删除")
+    private Integer isDelete;
+
+    @Serial
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
