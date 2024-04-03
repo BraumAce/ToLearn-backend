98e361aa8376e72b1640fa8f57372922ccab6213
BraumAce <1693717911@qq.com>
2024-04-03 19:15:08 +0800

    基本配置

---
+++ src/main/java/com/tolearn/properties/tolearnProperties.java
@@ -0,0 +1,18 @@
+package com.tolearn.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 配置
+ * @author BraumAce
+ */
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "tolearn")
+public class tolearnProperties {
+
+
+}
