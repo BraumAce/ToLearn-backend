33f72cbdb0bc575f9e3a38057a8c4288e1dadffb
BraumAce <1693717911@qq.com>
2024-04-04 16:27:11 +0800

    博文点赞服务

---
+++ src/main/java/com/tolearn/Service/BlogLikeService.java
@@ -0,0 +1,11 @@
+package com.tolearn.Service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tolearn.Model.Domain.BlogLike;
+
+/**
+ * 博文点赞服务
+ */
+public interface BlogLikeService extends IService<BlogLike> {
+
+}
