172ecb61e4b25338fceba42380093e036ec5d1ae
BraumAce <1693717911@qq.com>
2024-04-04 16:29:44 +0800

    用户队伍服务

---
+++ src/main/java/com/tolearn/Service/UserTeamService.java
@@ -0,0 +1,11 @@
+package com.tolearn.Service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tolearn.Model.Domain.UserTeam;
+
+/**
+* 用户队伍服务
+*/
+public interface UserTeamService extends IService<UserTeam> {
+
+}
