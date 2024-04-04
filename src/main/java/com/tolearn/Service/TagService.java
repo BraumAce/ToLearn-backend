cf217f54d5f57530cf5f626ec4e23a34a7bf4a78
BraumAce <1693717911@qq.com>
2024-04-04 16:28:55 +0800

    标签服务

---
+++ src/main/java/com/tolearn/Service/TagService.java
@@ -0,0 +1,11 @@
+package com.tolearn.Service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tolearn.Model.Domain.Tag;
+
+/**
+* 标签服务
+*/
+public interface TagService extends IService<Tag> {
+
+}
