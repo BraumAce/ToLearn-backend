5922c8b648016d53eef9e5c38cee69bd4a14da7e
BraumAce <1693717911@qq.com>
2024-04-04 20:19:29 +0800

    配置

--- src/main/java/com/toLearn/Properties/ToLearnProperties.java
+++ src/main/java/com/toLearn/Properties/ToLearnProperties.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @ConfigurationProperties(prefix = "tolearn")
-public class tolearnProperties {
+public class ToLearnProperties {
 
 
 }
