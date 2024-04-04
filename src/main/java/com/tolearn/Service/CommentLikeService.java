99fd6ae6c7fe68aca9c04d4b11900b08e630c8f0
BraumAce <1693717911@qq.com>
2024-04-04 16:28:09 +0800

    博文评论服务

---
+++ src/main/java/com/tolearn/Service/CommentLikeService.java
@@ -0,0 +1,11 @@
+package com.tolearn.Service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tolearn.Model.Domain.CommentLike;
+
+/**
+* 博文评论服务
+*/
+public interface CommentLikeService extends IService<CommentLike> {
+
+}
