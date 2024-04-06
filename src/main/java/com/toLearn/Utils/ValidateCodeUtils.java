a2621afd6f995c24c050521012d8aa83e911be4a
BraumAce <1693717911@qq.com>
2024-04-06 16:16:54 +0800

    验证码生成工具

---
+++ src/main/java/com/toLearn/Utils/ValidateCodeUtils.java
@@ -0,0 +1,27 @@
+package com.toLearn.Utils;
+
+import java.util.Random;
+
+import static com.toLearn.Constants.SystemConstants.MAXIMUM_VERIFICATION_CODE_NUM;
+import static com.toLearn.Constants.SystemConstants.MINIMUM_VERIFICATION_CODE_NUM;
+
+/**
+ * 验证码生成工具
+ * @author BraumAce
+ */
+public final class ValidateCodeUtils {
+    private ValidateCodeUtils() {
+    }
+
+    /**
+     * 生成验证代码
+     * @return {@link Integer}
+     */
+    public static Integer generateValidateCode() {
+        int code = new Random().nextInt(MAXIMUM_VERIFICATION_CODE_NUM); //生成随机数，最大为999999
+        if (code < MINIMUM_VERIFICATION_CODE_NUM) {
+            code = code + MINIMUM_VERIFICATION_CODE_NUM; //保证随机数为6位数字
+        }
+        return code;
+    }
+}
