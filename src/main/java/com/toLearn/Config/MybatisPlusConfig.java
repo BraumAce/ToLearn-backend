6d5419c800895ac58da46c3c8c1d2817644d208f
BraumAce <1693717911@qq.com>
2024-04-07 15:19:43 +0800

    MyBatis Plus配置

---
+++ src/main/java/com/toLearn/Config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.toLearn.Config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis Plus配置
+ * @author BraumAce
+ */
+@Configuration
+@MapperScan("com.toLearn.Mapper")
+public class MybatisPlusConfig {
+    /**
+     * MyBatis Plus拦截器
+     * @return {@link MybatisPlusInterceptor}
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        //新建分页插件
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
