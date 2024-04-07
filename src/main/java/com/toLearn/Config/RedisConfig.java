8d11867bf5cc4a031b9568b90822e90c8356bcfc
BraumAce <1693717911@qq.com>
2024-04-07 15:20:28 +0800

    Redis配置

---
+++ src/main/java/com/toLearn/Config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.toLearn.Config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis配置
+ * @author BraumAce
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    /**
+     * redisson客户
+     * @return {@link RedissonClient}
+     */
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        String address = "redis://" + host + ":" + port;
+        config.useSingleServer().setAddress(address);
+        return Redisson.create(config);
+    }
+}
